de8c60fb4d7cac6234b3944d9e7b60aa478b082a
Lokesh Yadav <Lokesh.Yadav@infostretch.com>
2020-02-28 17:41:54 +0530

    updated

---
+++ iis.ps1
@@ -0,0 +1,7 @@
+﻿param(
+    [string]$websitename="TestSite"
+)
+New-Item -ItemType directory "D:\$websitename" -force
+New-WebAppPool -Name $websitename -force
+New-WebSite -Name TestSite -Port 82 -HostHeader * -PhysicalPath "D:\$websitename" -ApplicationPool $websitename -force
+Copy-Item -Path "\\localhost\c$\Users\ninad.pantane\Azure DevOps\Agent\vsts-agent-win-x64-2.153.1\_work\1\s\SampleApp\obj\Release\netcoreapp3.1\PubTmp\Out\" -Destination "\\localhost\d$\$websitename" -Recurse
