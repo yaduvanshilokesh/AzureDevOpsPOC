b17bc6e68bb0a78ca991df625e62bbce200885b0
Lokesh Yadav <Lokesh.Yadav@infostretch.com>
2020-03-02 15:13:17 +0530

    final updated

--- iis.ps1
+++ iis.ps1
@@ -17,4 +17,4 @@ if($test.value -ne "Started"){
 if((Get-Website $websitename).state -ne "Started"){
 	New-WebSite -Name TestSite -Port 82 -HostHeader * -PhysicalPath "D:\AzureDevOpsPOC\$websitename" -ApplicationPool $websitename -force
 }
-Copy-Item -Path "\\localhost\c$\Users\lokesh.yadav\Desktop\Agent\_work\2\s\testwebapp\obj\Release\Package\PackageTmp" -Destination "\\localhost\d$\AzureDevOpsPOC\$websitename" -Recurse
+Copy-Item -Path "\\localhost\c$\Users\lokesh.yadav\Desktop\Agent\_work\2\s\testwebapp\obj\Release\Package\PackageTmp" -Destination "\\localhost\d$\AzureDevOpsPOC\$websitename" -Recurse -force
