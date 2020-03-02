0207f59748bad42e81a91178605654d91832272b
Lokesh Yadav <Lokesh.Yadav@infostretch.com>
2020-03-02 14:56:45 +0530

    updated

--- iis.ps1
+++ iis.ps1
@@ -1,7 +1,17 @@
 ﻿param(
     [string]$websitename="TestSite"
 )
-New-Item -ItemType directory "D:\$websitename" -force
-New-WebAppPool -Name $websitename -force
+New-Item -ItemType directory "D:\AzureDevOpsPOC\$websitename" -force
+
+try{
+	$test=Get-WebAppPoolState TestSite2
+}
+catch {
+	write-host "doesn't exist"
+}
+
+if($test.value -ne "Started"){
+	New-WebAppPool -Name $websitename -force
+}
 New-WebSite -Name TestSite -Port 82 -HostHeader * -PhysicalPath "D:\AzureDevOpsPOC\$websitename" -ApplicationPool $websitename -force
 Copy-Item -Path "\\localhost\c$\Users\lokesh.yadav\Desktop\Agent\_work\2\s\testwebapp\obj\Release\Package\PackageTmp" -Destination "\\localhost\d$\AzureDevOpsPOC\$websitename" -Recurse
